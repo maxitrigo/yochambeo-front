f0a7a529b37befa959a47326c227a796977883ae
maxitrigo <maxitrigo@gmail.com>
2024-10-04 14:51:50 -0300

    Cambios Front

--- src/components/listCards.jsx
+++ src/components/listCards.jsx
@@ -10,28 +10,34 @@ export const ListCards = ({  imgUrl,  title,  company,  location,  salary,  crea
 
   return (
     <div className="">
-    <div onClick={togleDetails} className="flex justify-between items-center border-t border-gray-300 pt-1 pb-1 w-full pr-4 pl-4">
+    <div onClick={togleDetails} className="flex justify-between items-center border-t pt-1 pb-1 w-full">
 
 
-      <div className="w-20 h-14 bg-gray-100 rounded-full flex items-center justify-center border-2 border-gray-300 mr-4">
+
+      <div className="w-1/4 bg-gray-100 rounded-full flex items-center justify-center border ml-2 mr-2">
       {imgUrl ? (
           <img src={imgUrl} alt={company} className="w-full h-full rounded-full object-cover" />
       ) : (
-          <span className="text-purple-700 text-2xl font-bold">Ych</span>
+          <span className=" text-2xl font-bold">Ych</span>
       )}
       </div>
 
-
       <div className="w-full">
 
-        <div className="">
+        <div className="w-1/2">
 
           <h1 className="text-l font-bold">{title}</h1>
           <p className="text-sm">{company}</p>
 
-          <div className="flex flex-row justify-start items-center">
-            <p className="text-sm text-gray-500 mr-2">🌎 {location}</p>
-            <p className="text-sm text-gray-500">💲{salary}</p>
+          <div className="flex flex-row justify-start items-center w-full">
+            <div className="flex flex-row justify-start items-center mr-2">
+            <p className="text-sm">🌎</p>
+            <p className="text-sm text-gray-500">{location}</p>
+            </div>
+            <div className="flex flex-row justify-start items-center">
+            <p className="text-sm font-bold mr-1"> $ </p>
+            <p className="text-sm text-gray-500">{salary}</p>
+            </div>
           </div>
 
         </div>
@@ -39,20 +45,21 @@ export const ListCards = ({  imgUrl,  title,  company,  location,  salary,  crea
       </div>
 
 
-      <div className="flex flex-row justify-start items-center">
-        <p className="text-sm text-gray-500">{createdAt} 📌</p>
+      <div className="flex flex-row justify-end items-center w-1/4 mr-2">
+        <p className="text-sm text-gray-500">{createdAt}</p>
+        <p className="text-sm text-gray-500">📌</p>
       </div>
 
     </div>
       {isOpen && (
-        <div onClick={togleDetails} className="mt-2 p-4  border border-gray-300 bg-gray-100">
-          <p className="font-bold text-lg mb-2 mt-2 text-purple-700">Descripción:</p>
+        <div onClick={togleDetails} className="mt-2 p-4 border-t">
+          <p className="font-bold text-lg mb-2 mt-2">Descripción:</p>
           <p>{description}</p>
-          <p className="font-bold text-lg mb-2 mt-4 text-purple-700">Requisitos:</p>
+          <p className="font-bold text-lg mb-2 mt-4">Requisitos:</p>
           <p>{requirements}</p>
-          <p className="font-bold text-lg mt-4 text-purple-700">Ubicación:</p>
+          <p className="font-bold text-lg mt-4">Ubicación:</p>
           <p>{location}</p>
-          <p className="font-bold text-lg mt-4 text-purple-700">Salario: </p>
+          <p className="font-bold text-lg mt-4">Salario: </p>
           <p>{salary}</p>
           <ContactButtons email={email} phone={phone} website={website} title={title} />
         </div>
