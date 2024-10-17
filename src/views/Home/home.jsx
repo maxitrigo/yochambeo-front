ebd2776b797d2ecbe74a4e7062c983469d8b4e22
maxitrigo <maxitrigo@gmail.com>
2024-10-17 15:19:13 -0300

    fix title

--- src/views/Home/home.jsx
+++ src/views/Home/home.jsx
@@ -18,14 +18,17 @@ export const Home = () => {
     return (
         <div className="flex flex-col items-center h-screen w-full">
             <div className="w-full md:w-3/4 lg:w-1/2">
-            <div className="flex flex-row justify-between pt-4 pl-4 pr-4 w-full">
-                <div className="flex flex-col">
-                <h1 className="text-3xl font-bold">YoChambeo</h1>
+            <div className="flex flex-row justify-between items-center pt-3 pl-3 pr-3 w-full">
+                <div className="flex items-center justify-start flex-grow">
+                    {/* <img src="../public/yochambeo.svg" alt="logo" className="w-20 mr-2" /> */}
+                    <h1 className="text-3xl font-bold">YoChambeo</h1>
                 </div>
-                <div className="flex flex-col justify-between">
-                    <button onClick={publish} className="text-white border bg-black px-2 py-2 rounded-2xl font-bold">Publicar</button>
+                <div>
+                    <button onClick={publish} className="text-white border bg-black px-2 py-2 rounded-2xl font-bold">
+                    Publicar
+                    </button>
                 </div>
-            </div>
+                </div>
             <div className="sticky top-0 z-10 w-full bg-white">
                 <SearchBox searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
             </div>
