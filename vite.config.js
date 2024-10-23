b5addef1a690fa73e75d1a76643a8881ddda054a
maxitrigo <maxitrigo@gmail.com>
2024-10-23 12:41:18 -0300

    sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://yochambeo.com',
+      routes: [
+        '/',
+        '/publish'
+      ],
+      autoGenerate: false
+    })
+  ],
 })
