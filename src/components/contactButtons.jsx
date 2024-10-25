8e4e7147efdd8d7e832bde27477a2a58207c654b
maxitrigo <maxitrigo@gmail.com>
2024-10-24 23:56:18 -0300

    fix: share formating

--- src/components/contactButtons.jsx
+++ src/components/contactButtons.jsx
@@ -12,7 +12,7 @@ const ContactButtons = ({ email, phone, website, title, description, requirement
         if (navigator.share) {
             navigator.share({
                 title: `Interés en el trabajo: ${title}`,
-                text: `Mirá este trabajo: ${title}, Descripcion: ${description}, Requisitos: ${requirements}, Podes enviar tu cv a ${email}`,
+                text: `Mirá este trabajo: ${title}\n\nDescripcion:\n${description}\nRequisitos:\n${requirements}\n\nPodes enviar tu cv a ${email}\n`,
                 url: window.location.href
             }).catch(console.error);
         } else {
